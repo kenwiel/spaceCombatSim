eed72384b8cf19abbb17d1d7d11b1d162978b27f
kenwiel <kenwiel@hotmail.de>
2017-05-14 19:11:43 +0200

    Initial Commit

---
+++ spaceCombatSim/src/main/java/space/funin/hiveQueenQuest/spaceCombatSim/App.java
@@ -0,0 +1,13 @@
+package space.funin.hiveQueenQuest.spaceCombatSim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
